5966ea63e1f78e591729e048dff24fcffd74d564
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2020-09-24 13:43:46 +0700

    finish laporan barang

--- page/cetak/data_master/barang/semua.php
+++ page/cetak/data_master/barang/semua.php
@@ -1,13 +1,67 @@
 <?php
-var_dump($_POST);
-var_dump("SELECT * FROM `tb_barang_data` INNER JOIN `tb_barang_kategori` ON `tb_barang_data`.`id_barang_kategori` = `tb_barang_kategori`.`id_barang_kategori`");
-die;
+$qselect = "";
+$qjoin = "";
+$qwhere = "";
+$title_laporan = "";
 
+if (isset($_POST['cetak'])) {
+    if (isset($_POST['tabel_all'])) {
+        $qselect = '*';
+        $qjoin .= " INNER JOIN `tb_barang_kategori` ON `tb_barang_data`.`id_barang_kategori` = `tb_barang_kategori`.`id_barang_kategori` ";
+    } else {
+        $qselect .= "`id_barang_data`";
+        if (isset($_POST['tabel_nama'])) $qselect .= ",`barang_data_nama`";
+        if (isset($_POST['tabel_harga_beli'])) $qselect .= ",`barang_data_harga_beli`";
+        if (isset($_POST['tabel_harga_jual'])) $qselect .= ",`barang_data_harga_jual`";
+        if (isset($_POST['tabel_gambar'])) $qselect .= ",`barang_data_gambar`";
+        if (isset($_POST['tabel_tanggal'])) $qselect .= ",`barang_data_tanggal`";
+        if (isset($_POST['tabel_kategori'])) {
+            $qselect .= ",`tb_barang_data`.`id_barang_kategori`, `tb_barang_kategori`.`id_barang_kategori`, `tb_barang_kategori`.`barang_kategori_nama`";
+            $qjoin .= " INNER JOIN `tb_barang_kategori` ON `tb_barang_data`.`id_barang_kategori` = `tb_barang_kategori`.`id_barang_kategori` ";
+        }
+    }
 
+    if (!isset($_POST['kategori_all'])) {
+        $querykategori = query('SELECT * FROM tb_barang_kategori');
+        $qkategori = "";
+        if ($querykategori) {
+            foreach ($querykategori as $kategori) {
+                if (isset($_POST['kategori_' . $kategori['id_barang_kategori']])) {
+                    if (!isset($_POST['tanggal_all'])) {
+                        if ($qwhere == "") $qwhere .= "WHERE (";
+                        else $qwhere .= " OR ";
+                    } else {
+                        if ($qwhere == "") $qwhere .= "WHERE ";
+                        else $qwhere .= " OR ";
+                    }
+                    if ($title_laporan == "") $title_laporan = "Laporan Barang<br>Kategori: " . $kategori['barang_kategori_nama'];
+                    else $title_laporan .= ", " . $kategori['barang_kategori_nama'];
 
+                    $id_kategori = $kategori['id_barang_kategori'];
+                    $qwhere .= " `tb_barang_data`.`id_barang_kategori` = '$id_kategori' ";
+                }
+            }
+        }
+    }
+
+    if (!isset($_POST['tanggal_all'])) {
+        $dari = $_POST['tanggal_dari'];
+        $sampai = $_POST['tanggal_sampai'];
+        if ($title_laporan == "") $title_laporan = "Laporan Barang<br>Tanggal: Dari " . $dari . " Sampai " . $sampai;
+        else $title_laporan .= "<br>Tanggal: Dari " . $dari . " Sampai " . $sampai;
+
+        if ($qwhere == "") $qwhere .= "WHERE ";
+        else $qwhere .= ") AND ";
+        $qselect .= ",`barang_data_tanggal`";
+        $qwhere .= " `tb_barang_data`.`barang_data_tanggal` BETWEEN '$dari' AND '$sampai'";
+    }
+}
+
+$qbuilder = "SELECT " . $qselect . " FROM `tb_barang_data` " . $qjoin . $qwhere;
 $nomor = 1;
-$datas = query("SELECT * FROM `tb_barang_data` INNER JOIN `tb_barang_kategori` ON `tb_barang_data`.`id_barang_kategori` = `tb_barang_kategori`.`id_barang_kategori`");
+$datas = query($qbuilder);
 ?>
+
 <style>
     h3 {
         text-align: center;
@@ -19,33 +73,45 @@ $datas = query("SELECT * FROM `tb_barang_data` INNER JOIN `tb_barang_kategori` O
         font: 11pt "Tahoma", "Helvetica";
     }
 </style>
+<?php if (isset($_POST['cetak'])) : ?>
+    <?php if ($title_laporan == "") {
+        echo '<h3>Lapora Semua Barang</h3>';
+    } else {
+        echo '<h3>' . $title_laporan . '</h3>';
+    } ?>
+    <table>
+        <thead>
+            <tr>
+                <th style="text-align: center;">No</th>
+                <?php
+                if (isset($_POST['tabel_nama'])) echo '<th style="text-align: center;">Nama barang</th>';
+                if (isset($_POST['tabel_harga_beli'])) echo '<th style="text-align: center;">Harga beli</th>';
+                if (isset($_POST['tabel_harga_jual'])) echo '<th style="text-align: center;">Harga jual</th>';
+                if (isset($_POST['tabel_stok'])) echo '<th style="text-align: center;">Stok</th>';
+                if (isset($_POST['tabel_gambar'])) echo '<th style="text-align: center;">Gambar</th>';
+                if (isset($_POST['tabel_tanggal'])) echo '<th style="text-align: center;">Tgl Daftar</th>';
+                if (isset($_POST['tabel_kategori'])) echo '<th style="text-align: center;">Kategori</th>';
+                ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if ($datas) : ?>
+                <?php foreach ($datas as $data) : ?>
+                    <tr>
+                        <td style="text-align: center;"><?php echo $nomor++; ?></td>
+                        <?php
+                        if (isset($_POST['tabel_nama'])) echo '<td>' . $data["barang_data_nama"] . '</td>';
+                        if (isset($_POST['tabel_harga_beli'])) echo '<td style="text-align:right;">Rp. ' . number_format($data["barang_data_harga_beli"], 0, ',', '.') . '</td>';
+                        if (isset($_POST['tabel_harga_jual'])) echo '<td style="text-align:right;">Rp. ' . number_format($data["barang_data_harga_jual"], 0, ',', '.') . '</td>';
+                        if (isset($_POST['tabel_stok'])) echo '<td style="text-align:right;">' . getStokBarang($data["id_barang_data"]) . '</td>';
+                        if (isset($_POST['tabel_gambar'])) echo '<td style="text-align:center;"><img style="width: 80px; height: 80px; object-fit:contain;" src="../../images/master_data_barang/' . $data["barang_data_gambar"] . '" style="width: 100px;" alt="..." class="img-thumbnail"></td>';
+                        if (isset($_POST['tabel_tanggal'])) echo '<td>' . $data["barang_data_tanggal"] . '</td>';
+                        if (isset($_POST['tabel_kategori'])) echo '<td>' . $data["barang_kategori_nama"] . '</td>';
+                        ?>
+                    </tr>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </tbody>
+    </table>
 
-<h3>Lapora Semua Barang Per Tanggal</h3>
-<table>
-    <thead>
-        <tr>
-            <th style="text-align: center;">No</th>
-            <th style="text-align: center;">Nama barang</th>
-            <th style="text-align: center;">Harga beli</th>
-            <th style="text-align: center;">Harga jual</th>
-            <th style="text-align: center;">Stok</th>
-            <th style="text-align: center;">Gambar</th>
-            <th style="text-align: center;">Kategori</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php if ($datas) : ?>
-            <?php foreach ($datas as $data) : ?>
-                <tr>
-                    <td style="white-space: nowrap; text-align: center;"><?php echo $nomor++; ?></td>
-                    <td style="white-space: nowrap;"><?php echo $data['barang_data_nama']; ?></td>
-                    <td style="white-space: nowrap; text-align:right;">Rp. <?php echo number_format($data['barang_data_harga_beli'], 0, ',', '.'); ?></td>
-                    <td style="white-space: nowrap; text-align:right;">Rp. <?php echo number_format($data['barang_data_harga_jual'], 0, ',', '.'); ?></td>
-                    <td style="white-space: nowrap; text-align:right;"><?php echo getStokBarang($data['id_barang_data']); ?></td>
-                    <td style="white-space: nowrap; text-align:center;"><img style="width: 80px; height: 80px; object-fit:contain;" src="../../images/master_data_barang/<?php echo $data['barang_data_gambar']; ?>" style="width: 100px;" alt="..." class="img-thumbnail"></td>
-                    <td style="white-space: nowrap;"><?php echo $data['barang_kategori_nama']; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </tbody>
-</table>
+<?php endif; ?>
--- page/data_master/barang/form_laporan_barang.php
+++ page/data_master/barang/form_laporan_barang.php
@@ -37,6 +37,10 @@ $kategori = query('SELECT * FROM tb_barang_kategori');
                                         <td style="text-align:right;"><input type="checkbox" class="checkbox checkbox_tabel" id="tabel_kategori" checked="" name="tabel_kategori"></td>
                                     </tr>
                                     <tr>
+                                        <td style="white-space:nowrap"><label for="tabel_tanggal" class="form-check-label">Tanggal</label></td>
+                                        <td style="text-align:right;"><input type="checkbox" class="checkbox checkbox_tabel" id="tabel_tanggal" checked="" name="tabel_tanggal"></td>
+                                    </tr>
+                                    <tr>
                                         <td style="white-space:nowrap"><label for="tabel_gambar" class="form-check-label">Gambar</label></td>
                                         <td style="text-align:right;"><input type="checkbox" class="checkbox checkbox_tabel" id="tabel_gambar" name="tabel_gambar"></td>
                                     </tr>
@@ -58,15 +62,17 @@ $kategori = query('SELECT * FROM tb_barang_kategori');
                         <div class="col-md-4">
                             <div class="container-fluid">
                                 <h4 style="padding:3px 0; margin:0; text-align:center;">Tanggal Daftar</h4>
-                                <div class="form-group">
-                                    <label>Dari Tanggal</label>
-                                    <input class="form-control" readonly="" type="date" name="tanggal_dari" id="tanggal_dari" value="<?= date("yy-m-d"); ?>" />
+                                <div id="tanggal_tabel">
+                                    <div class="form-group">
+                                        <label>Dari Tanggal</label>
+                                        <input class="form-control" type="date" name="tanggal_dari" id="tanggal_dari" value="<?= date("yy-m-d"); ?>" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Sampai Tanggal</label>
+                                        <input class="form-control" type="date" name="tanggal_sampai" id="tanggal_sampai" value="<?= date("yy-m-d"); ?>" />
+                                    </div>
+                                    <hr>
                                 </div>
-                                <div class="form-group">
-                                    <label>Sampai Tanggal</label>
-                                    <input class="form-control" readonly="" type="date" name="tanggal_sampai" id="tanggal_sampai" value="<?= date("yy-m-d"); ?>" />
-                                </div>
-                                <hr>
                                 <table width="100%">
                                     <tr>
                                         <td style="white-space:nowrap"><label for="tanggal_all" class="form-check-label">Semua Tanggal</label></td>
@@ -84,7 +90,7 @@ $kategori = query('SELECT * FROM tb_barang_kategori');
                                         foreach ($kategori as $k) { ?>
                                             <tr>
                                                 <td style="white-space:nowrap"><label for="kategori_<?php echo $k['id_barang_kategori']; ?>" class="form-check-label"><?php echo $k['barang_kategori_nama']; ?></label></td>
-                                                <td style="text-align:right;"><input type="checkbox" class="checkbox checkbox_kategori" id="kategori_<?php echo $k['id_barang_kategori']; ?>" name="kategori_<?php echo $k['id_barang_kategori']; ?>"></td>
+                                                <td style="text-align:right;"><input type="checkbox" class="checkbox checkbox_kategori" id="kategori_<?php echo $k['id_barang_kategori']; ?>" name="kategori_<?php echo $k['id_barang_kategori']; ?>" checked=""></td>
                                             </tr>
                                     <?php }
                                     } ?>
@@ -96,8 +102,8 @@ $kategori = query('SELECT * FROM tb_barang_kategori');
                                 </table>
                                 <table width="100%" onclick="kategoriCapture()">
                                     <tr>
-                                        <td style="white-space:nowrap"><label for="kategori_all" class="form-check-label">Pilih Semua</label></td>
-                                        <td style="text-align:right;"><input type="checkbox" class="checkbox" id="kategori_all" name="kategori_all"></td>
+                                        <td style="white-space:nowrap"><label for="kategori_all" class="form-check-label">Semua Kategori</label></td>
+                                        <td style="text-align:right;"><input type="checkbox" class="checkbox" id="kategori_all" name="kategori_all" checked=""></td>
                                     </tr>
                                 </table>
                                 <hr>
@@ -107,7 +113,7 @@ $kategori = query('SELECT * FROM tb_barang_kategori');
                     <br>
                     <div class="row">
                         <div class="col-md-12">
-                            <button class="btn btn-primary btn-block" type="submit"><i class="fa fa-print"></i> Cetak Laporan</button>
+                            <button class="btn btn-primary btn-block" type="submit" name="cetak"><i class="fa fa-print"></i> Cetak Laporan</button>
                         </div>
                     </div>
                 </form>
@@ -120,10 +126,18 @@ $kategori = query('SELECT * FROM tb_barang_kategori');
     function tableCapture() {
         const tabel_checkbox = document.querySelectorAll('.checkbox_tabel');
         let tabel_all_btn = document.querySelector('#tabel_all');
+
+        const tabelVisib = (bool = true) => {
+            const tabel_tabel = document.querySelector('#tabel_tabel');
+            if (bool) tabel_tabel.removeAttribute('hidden');
+            else tabel_tabel.setAttribute('hidden', '');
+        }
+
         const tabel_checkedall = () => {
             tabel_checkbox.forEach(element => {
                 element.checked = true;
             });
+            tabelVisib(false);
         }
 
         const tabel_periksa = () => {
@@ -131,7 +145,10 @@ $kategori = query('SELECT * FROM tb_barang_kategori');
             tabel_checkbox.forEach(element => {
                 if (!element.checked) cek = false;
             });
-            tabel_all_btn.checked = cek;
+            if (cek) {
+                tabelVisib(false);
+                tabel_all_btn.checked = cek;
+            }
         }
 
         tabel_checkbox.forEach(element => {
@@ -142,6 +159,7 @@ $kategori = query('SELECT * FROM tb_barang_kategori');
 
         tabel_all_btn.addEventListener('change', function() {
             if (this.checked) tabel_checkedall();
+            else tabelVisib();
         });
     }
 
@@ -185,15 +203,22 @@ $kategori = query('SELECT * FROM tb_barang_kategori');
         });
     }
 
-    function cektanggal(data) {
+
+    function cektanggal(data = document.querySelector('#tanggal_all')) {
         let tanggal_dari = document.querySelector('#tanggal_dari');
         let tanggal_sampai = document.querySelector('#tanggal_sampai');
+        const tanggal_tabel = document.querySelector('#tanggal_tabel');
         if (data.checked) {
             tanggal_dari.setAttribute('readonly', '');
             tanggal_sampai.setAttribute('readonly', '');
+            tanggal_tabel.setAttribute('hidden', '');
         } else {
             tanggal_dari.removeAttribute('readonly');
             tanggal_sampai.removeAttribute('readonly');
+            tanggal_tabel.removeAttribute('hidden');
         }
     }
+    cektanggal();
+    if (!document.querySelector('#kategori_all').checked) document.querySelector('#kategori_tabel').removeAttribute('hidden');
+    else document.querySelector('#kategori_tabel').setAttribute('hidden', '');
 </script>
